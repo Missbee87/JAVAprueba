d351b59c91b12c79c3593ae56a74590730aa67f3
Belen <blnmolina@gmail.com>
2023-03-08 15:16:04 -0300

    Codigo con estructura

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Celeste
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int numeroInicio=5;
+        
+        for(int numeroFin=14;numeroFin>numeroInicio;numeroFin-=2){
+            System.out.println(numeroFin);
+        }
+         
+        }
+}
+    
+
